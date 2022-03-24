71d72c5efb168e5d38621eea3ad14f8e2a2d5e2a
Amare Teklay Hailu <amareteklay@gmail.com>
2022-03-24 10:43:02 +0000

    replace if with while loop to repeat validation

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
+from termcolor import colored
 from questions import questionBank
 
-print('Welcome')
+print(colored('Welcome', 'green'))
 print('SDG Quiz')
 
 # Create a list of question dictionaries.
@@ -13,37 +14,36 @@ class Question:
     Blueprint of questions.
     Takes a list of dictionaries and displays questions and choices.
     """
-    def __init__(self, dict_list):
+    def __init__(self, question_index, dict_list):
         """"
         Initialize a question object
         """
+        self.question_index = question_index + 1
         self.question = dict_list['question']
         self.choice_a = dict_list['choices']['A']
         self.choice_b = dict_list['choices']['B']
         self.choice_c = dict_list['choices']['C']
         self.choice_d = dict_list['choices']['D']
         self.answer = dict_list['answer']
-        self.user_answer = self.get_answer()
-
-
-    def ask_question(self):
-        """
-        Display question prompt and choices
-        """
-        print(self.question, '\n')
-        print('A.', self.choice_a, '\n')
-        print('B.', self.choice_b, '\n')
-        print('C.', self.choice_c, '\n')
-        print('D.', self.choice_d, '\n')
-
+        self.user_answer = ''
 
     def get_answer(self):
         """
         Get answer as user input
         """
-        self.ask_question()
         self.user_answer = input('Enter your answer.\n')
         return self.user_answer
+
+    def ask_question(self):
+        """
+        Display question prompt and choices
+        """
+        print(f'{self.question_index}. {self.question} \n')
+        print('A.', self.choice_a, '\n')
+        print('B.', self.choice_b, '\n')
+        print('C.', self.choice_c, '\n')
+        print('D.', self.choice_d, '\n')
+        self.user_answer = self.get_answer()
 
 
 class User:
@@ -58,9 +58,12 @@ class User:
         self.score = 0
         self.given_answer = ''
 
-
     def show_question(self, question_index):
-        self.question_object = Question(qsns[question_index])
+        """
+        Display questions and choices
+        """
+        self.question_object = Question(question_index, qsns[question_index])
+        self.question_object.ask_question()
         self.given_answer = self.question_object.user_answer
         return self.given_answer
 
@@ -71,19 +74,16 @@ class User:
         self.score += 1
         return self.score
 
-
-    def validate_answer(self, given_answer):
+    def validate_answer(self):
         """
         Get answer from user and validate.
         Input answer can only be one of a, b, c or d.
         Change input to lower case and look it up in the list.
         """
-        if given_answer.lower() not in ['a', 'b', 'c', 'd']:
-            print(given_answer.lower())
+        while self.given_answer.lower() not in ['a', 'b', 'c', 'd']:
             print('You answer should be one of: a, b, c or d')
             self.given_answer = input('Try again.\n')
         return self.given_answer
-
 
     def evaluate_answer(self, qsn, given_answer):
         """
@@ -92,10 +92,10 @@ class User:
         Display feedback and if answer is correct, increment score.
         """
         if given_answer.upper() == qsns[qsn]['answer']:
-            print('Correct! Well done.')
+            print(colored('Correct! Well done.', 'green'))
             self.update_score()
         else:
-            print('Incorrect')
+            print(colored('Incorrect', 'red'))
         print(self.score)
 
 
@@ -103,11 +103,11 @@ def take_quiz():
     """
     Start the quiz. Get answer, give feedback and show next question.
     """
-    #Create an instance of the user class.
+    # Create an instance of the user class.
     user = User()
     for i in range(3):
         user.show_question(i)
-        user.validate_answer(user.given_answer)
+        user.validate_answer()
         user.evaluate_answer(i, user.given_answer)
     print(f'You have answered {user.score} questions out of {len(qsns)}.')
 
