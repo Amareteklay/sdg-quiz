576ec66be4502770fc225395f1ee550813d09a38
Amare Teklay Hailu <amareteklay@gmail.com>
2022-04-02 12:35:39 +0000

    replaced fake questions with real SDG questions

--- questions.py
+++ questions.py
@@ -1,74 +1,87 @@
 def questionBank():
     questions = [
-        {'question': 'How many SDGS are there?',
+
+        {'question': 'SDG stands for:',
+         'choices': {
+             'A': 'Sustainable Department Goals',
+             'B': 'Sustainable Development Goals',
+             'C': 'Sustainable Development Governance',
+             'D': 'Sustainable Development Grants'},
+         'answer': 'B'},
+
+        {'question': 'When was the 2030 Agenda for Sustainable Development adopted?',
+         'choices': {
+             'A': '2010',
+             'B': '2015',
+             'C': '2016',
+             'D': '2020'},
+         'answer': 'B'},
+
+         {'question': 'How many SDGS are there?',
          'choices': {
              'A': 15,
              'B': 27,
              'C': 17,
              'D': 30},
          'answer': 'C'},
-        {'question': 'SDG stands for:',
+
+        {'question': 'Which is not a dimension of Sustainable Development?',
          'choices': {
-             'A': 'Sustainable Department Goals',
-             'B': 'Sustainable Development Goals',
-             'C': 'Sustainable Development Governance',
-             'D': 'Sustainable Development Grants'},
+             'A': 'Economic',
+             'B': 'Social',
+             'C': 'Environmental',
+             'D': 'None'},
+         'answer': 'D'},
+
+        {'question': '''Goal 17 seeks to Strengthen the means of implementation and revitalize the Global Partnership for Sustainable Development. 
+What are these means of implementation?''',
+         'choices': {
+             'A': 'Finance',
+             'B': 'Technology',
+             'C': 'Capacity-building',
+             'D': 'All'},
+         'answer': 'D'},
+
+        # 2 questions copied/adapted from https://quizizz.com/admin/quiz/5c6c4413206101001a84dce7/sustainable-development-goals
+        {'question': 'Which is not an obstacle to achieving the Sustainable Development Goals?',
+         'choices': {
+             'A': 'Capital cities',
+             'B': 'Differences in income',
+             'C': 'Ecological problems',
+             'D': 'Using more than we produce or grow'},
+         'answer': 'A'},
+
+        {'question': 'Which SDG aims to Conserve and sustainably use the oceans, seas and marine resources for Sustainable Development?',
+         'choices': {
+             'A': 'Life of land',
+             'B': 'Life below water',
+             'C': 'Climate action',
+             'D': 'No poverty'},
          'answer': 'B'},
-        {'question': 'SDG stands for:',
+
+        {'question': 'Which statement is correct?',
          'choices': {
-             'A': 'Sustainable Department Goals',
-             'B': 'Sustainable Development Goals',
-             'C': 'Sustainable Development Governance',
-             'D': 'Sustainable Development Grants'},
+             'A': 'Sustainable development is the same as economic development',
+             'B': 'The 2030 Agenda for Sustainable Development has 17 goals and 169 targets',
+             'C': 'Each of the 17 goals in the SDGs has the same number of targets',
+             'D': 'Developed countries do not have to include SDGs in their national policies because they are already developed'},
          'answer': 'B'},
-        {'question': 'SDG stands for:',
+
+        {'question': 'Which goal aims to achieve sustainable management and efficient use of natural resources by 2030?',
          'choices': {
-             'A': 'Sustainable Department Goals',
-             'B': 'Sustainable Development Goals',
-             'C': 'Sustainable Development Governance',
-             'D': 'Sustainable Development Grants'},
-         'answer': 'B'},
-        {'question': 'SDG stands for:',
+             'A': 'Climate actions',
+             'B': 'Clean water and sanitation',
+             'C': 'Responsible consumption and production',
+             'D': 'Life on land'},
+         'answer': 'C'},
+         
+        {'question': '''Based on the 2022 Climate Change Performance Index, ______ was 
+ranked as the country with the highest achievement in climate protection, followed by _______.''',
          'choices': {
-             'A': 'Sustainable Department Goals',
-             'B': 'Sustainable Development Goals',
-             'C': 'Sustainable Development Governance',
-             'D': 'Sustainable Development Grants'},
-         'answer': 'B'},
-        {'question': 'SDG stands for:',
-         'choices': {
-             'A': 'Sustainable Department Goals',
-             'B': 'Sustainable Development Goals',
-             'C': 'Sustainable Development Governance',
-             'D': 'Sustainable Development Grants'},
-         'answer': 'B'},
-        {'question': 'SDG stands for:',
-         'choices': {
-             'A': 'Sustainable Department Goals',
-             'B': 'Sustainable Development Goals',
-             'C': 'Sustainable Development Governance',
-             'D': 'Sustainable Development Grants'},
-         'answer': 'B'},
-        {'question': 'SDG stands for:',
-         'choices': {
-             'A': 'Sustainable Department Goals',
-             'B': 'Sustainable Development Goals',
-             'C': 'Sustainable Development Governance',
-             'D': 'Sustainable Development Grants'},
-         'answer': 'B'},
-        {'question': 'SDG stands for:',
-         'choices': {
-             'A': 'Sustainable Department Goals',
-             'B': 'Sustainable Development Goals',
-             'C': 'Sustainable Development Governance',
-             'D': 'Sustainable Development Grants'},
-         'answer': 'B'},
-        {'question': 'SDG stands for:',
-         'choices': {
-             'A': 'Sustainable Department Goals',
-             'B': 'Sustainable Development Goals',
-             'C': 'Sustainable Development Governance',
-             'D': 'Sustainable Development Grants'},
-         'answer': 'B'}
+             'A': 'Denmark, Sweden',
+             'B': 'Sweden, Denmark',
+             'C': 'Norway, Sweden',
+             'D': 'Sweden, Norway'},
+         'answer': 'A'}
     ]
     return questions
